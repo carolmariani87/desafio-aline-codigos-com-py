3704ae3d5ad47debecfeb4bf0f8eeb72faa71531
carolmariani87 <lanature.contato@gmail.com>
2025-06-18 16:27:01 -0300

    fix: correct variable name in print statement

---
+++ repet_text.py
@@ -0,0 +1,8 @@
+#solicitar uma string e um numetro inteiro como entrada 
+import string
+
+
+info = input('Digite uma string: ')
+numero = int(input('Digite um número inteiro: '))   
+
+print ((string +" ")*numero)
